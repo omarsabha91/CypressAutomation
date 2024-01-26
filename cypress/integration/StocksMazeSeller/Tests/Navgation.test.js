4ef560f5d8f2754db8a61b7c0d060e4b4f7cf82a
omarsabha91 <o.sabha1@gmail.com>
2024-01-26 16:45:36 -0600

    parametarizing nav and verify

--- cypress/integration/StocksMazeSeller/Tests/Navgation.test.js
+++ cypress/integration/StocksMazeSeller/Tests/Navgation.test.js
@@ -1,11 +1,11 @@
 
 import SignInPage from '../Pages/SignInPage';
 import DashBoardPage from '../Pages/DashBoardPage';
-import ProductsPage from '../Pages/ProductsPage';
 import OrdersPage from '../Pages/OrdersPage';
 import NotificationsPage from "../Pages/NotifcationsPage";
 import PaymentsPage from "../Pages/PaymentsPage";
 import SettingsPage from "../Pages/SettingsPage";
+import ProductsPage from "../Pages/ProductsPage";
 describe('StocksMazeSeller Navigations Test', () => {
     const signInPage = new SignInPage();
     const dashBoardPage = new DashBoardPage();
@@ -13,27 +13,33 @@ describe('StocksMazeSeller Navigations Test', () => {
     const settingsPage = new SettingsPage();
     const paymentsPage = new PaymentsPage();
     const notificationsPage = new NotificationsPage();
+    const productsPage = new ProductsPage();
+
+    function navigateAndVerify(page, navItem, expectedUrl, expectedTitle) {
+        page.navBar.clickNavItem(navItem);
+        cy.url().should('include', expectedUrl);
+        cy.title().should('include', expectedTitle);
+    }
+
     it('should Navigate to all pages successfully', () => {
+
         signInPage.visit();
         signInPage.typeUsername(Cypress.env('username'));
         signInPage.typePassword(Cypress.env('password'));
         signInPage.submit();
-        cy.url().should('include', '/dashboard');
-        cy.title().should('include', 'Dashboard');
-        dashBoardPage.navBar.clickNavItem(dashBoardPage.navBar.NAV_ITEMS.NOTIFICATIONS);
-        cy.url().should('include', '/notifications');
-        cy.title().should('include', 'Notifications');
-        notificationsPage.navBar.clickNavItem(notificationsPage.navBar.NAV_ITEMS.PAYMENTS);
-        cy.url().should('include', '/payments');
-        cy.title().should('include', 'Payments');
-        paymentsPage.navBar.clickNavItem(paymentsPage.navBar.NAV_ITEMS.SETTINGS);
-        cy.url().should('include', '/settings');
-        cy.title().should('include', 'Settings');
-        settingsPage.navBar.clickNavItem(settingsPage.navBar.NAV_ITEMS.ORDERS);
-        cy.url().should('include', '/orders');
-        cy.title().should('include', 'Orders');
-        ordersPage.navBar.clickNavItem(ordersPage.navBar.NAV_ITEMS.PRODUCTS);
-        cy.url().should('include', '/products');
-        cy.title().should('include', 'Products');
+
+        const navigationSteps = [
+            { page: dashBoardPage, navItem: dashBoardPage.navBar.NAV_ITEMS.NOTIFICATIONS, expectedUrl: '/notifications', expectedTitle: 'Notifications' },
+            { page: notificationsPage, navItem: notificationsPage.navBar.NAV_ITEMS.PRODUCTS, expectedUrl: '/products', expectedTitle: 'Products' },
+            { page: productsPage, navItem: productsPage.navBar.NAV_ITEMS.ORDERS, expectedUrl: '/orders', expectedTitle: 'Orders' },
+            { page: ordersPage, navItem: ordersPage.navBar.NAV_ITEMS.PAYMENTS, expectedUrl: '/payments', expectedTitle: 'Payments' },
+            { page: paymentsPage, navItem: paymentsPage.navBar.NAV_ITEMS.SETTINGS, expectedUrl: '/settings', expectedTitle: 'Settings' },
+            { page: settingsPage, navItem: settingsPage.navBar.NAV_ITEMS.HOME, expectedUrl: '/dashboard', expectedTitle: 'Dashboard' }
+        ];
+
+        navigationSteps.forEach(step => {
+            navigateAndVerify(step.page, step.navItem, step.expectedUrl, step.expectedTitle);
+        });
+
     });
 });
